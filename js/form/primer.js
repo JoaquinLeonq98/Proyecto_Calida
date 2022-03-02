279c151bb1123d11232a1a93bbdb39085100e1b1
kingaurelio1 <aurelior997@ciencias.unam.mx>
2022-03-02 12:06:04 -0600

    Modifique la función de validaciones

--- js/form/primer.js
+++ js/form/primer.js
@@ -5,28 +5,42 @@ function validacionForm(nombre,correo,telefono,mensaje){
     let nombreValor = nombre.value;
     let telefonoValor = telefono.value;
     let mensajeValor = mensaje.value;
+    let estado = true;
 
     
     if(correoValor === ""){
         correo.classList.add("is-invalid");
+        let estado = false;
     }else{
         correo.classList.add("is-valid");
     }
     if(nombreValor === ""){
         nombre.classList.add("is-invalid");
+        let estado = false;
     }else{
         nombre.classList.add("is-valid");
     }
     if(telefonoValor === ""){
         telefono.classList.add("is-invalid");
+        let estado = false;
     }else{
         telefono.classList.add("is-valid");
     }
     if(mensajeValor === ""){
-        alert("Agrega un mensaje");
         mensaje.classList.add("is-invalid");
+        let estado = false;
     }else{
         mensaje.classList.add("is-valid");
     }
-
+    return estado;
 }
+
+formRespuesta.addEventListener("submit",(evento)=>{
+
+    evento.preventDefault();
+    validacionForm(correo,nombre,telefono,mensaje);
+    let precioValor = precio.value;
+    let nombreValor = nombre.value;
+   let nuevaTarjeta= crearTarjeta(nombreValor,precioValor);
+   ancla.innerHTML = nuevaTarjeta;
+})
