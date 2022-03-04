c785be9343d05ddc7ed2d661b5af71875170b144
lbca <calberto.lopezbenitez@gmail.com>
2022-03-03 22:53:20 -0600

    Se crea el footer JS a partir del avance footer.html, se agrega estylos a footer.css

--- js/footer.js
+++ js/footer.js
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-  <link rel="stylesheet" href="/css/main.css"> 
-    <title>Document</title>
-</head>
-<body>
 
-</body>
-<footer>
-    <!-- Footer -->
+ // contenidoFooter es una constante que almacena en acentos graves el HTML del footer global
+ const contenidoFooter = `
+
+ <!-- Footer -->
     <footer class="text-center text-lg-start text-muted contenido bajarFooter">
         <!-- Línea arriba -->
         <section
@@ -31,10 +21,10 @@
             <!-- Lado izq (logo) -->
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 <!-- Content -->
-                <img class="mb-4" src="/img/calida-logo-vf-color-transparent.png" alt="logo" height="100px">
+                <img class="mb-4" src="../img/calida-logo-vf-color-transparent.png" alt="logo" height="100px">
                 <p class="text-white">Tu tienda de plantas y macetas a precios accesibles</p>
-                <a href="https://www.instagram.com/calidamx/" target="_blank"><img src="/img/iconmonstr-instagram-13.svg" alt="logo insta" height="35px"></a>
-                <a href="https://www.facebook.com/CalidaMX/" class="ml-3" target="_blank"><img src="/img/iconmonstr-facebook-3.svg" alt="logo face" height="35px"></a>
+                <a href="https://www.instagram.com/calidamx/" target="_blank"><img src="../img/iconmonstr-instagram-13.svg" alt="logo insta" height="35px"></a>
+                <a href="https://www.facebook.com/CalidaMX/" class="ml-3" target="_blank"><img src="../img/iconmonstr-facebook-3.svg" alt="logo face" height="35px"></a>
             </div>
             <!-- lado izq (logo) -->
             
@@ -79,8 +69,11 @@
         </div>
     <!--Termina Footer-->
 
-    
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
     </footer>
-</html>
+   
+`;
+// A la constante "ancla" se le da ubicacion por medio del id="anclaFooter"
+const ancla = document.getElementById("anclaFooter");
+
+// Asigna el contenido de "contenidoFooter" y devuelve la sintaxis HTML con un "innerHTML" a la constante "anclaF"
+ancla.innerHTML= contenidoFooter;
