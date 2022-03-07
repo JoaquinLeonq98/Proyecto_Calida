27dfccf51d099877abd99b31b8126858db014543
fernanda3091 <maria.fernanda.rivera.medina30@gmail.com>
2022-03-07 13:53:43 -0600

    se agrega link de aurelio en navbar

--- js/navBar.js
+++ js/navBar.js
@@ -23,7 +23,7 @@
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="https://www.linkedin.com/in/calbertolb/">Carlos Alberto López Benítez</a>
                             <a class="dropdown-item" href="#">Joaquín León Quero</a>
-                            <a class="dropdown-item" href="#">Aurelio Reyes Sanchez</a>
+                            <a class="dropdown-item" href="https://www.linkedin.com/in/aurelio-reyes-s%C3%A1nchez/">Aurelio Reyes Sanchez</a>
                             <a class="dropdown-item" href="https://www.linkedin.com/in/maría-fernanda-rivera-medina-77aaa522b/">Maria Fernanda Rivera Medina</a>
                             <a class="dropdown-item" href="https://www.linkedin.com/in/crismoute/">Cristian Mota Ramirez</a>
                             <a class="dropdown-item" href="#">Manuel Alberto De La Torre Piña</a>
