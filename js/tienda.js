a5c65ad23c8c0d73b890cb97a1a7ce6023432f14
nqf <naomi.quinterofl@gmail.com>
2022-03-10 18:29:35 -0600

    Se edito tienda html y tienda js

--- js/tienda.js
+++ js/tienda.js
@@ -1,18 +1,18 @@
 function addItem(item){
     const itemHTML = 
-'<div class="row-lg-4  m-2 d-flex align-items-stretch"">'+
-'<div class="card">'+
-'<img src="'+item.img +'" class="card-img-top mx-auto" style="width: 18rem" alt="...">' +
-'<div class="card-body">'+   
-  '<h5 class="card-title">'+item.name +'</h5>'+
-  '<p class="card-text">'+item.price +'</p>'+
-  '<p class="card-text" style="width: 18rem">'+item.description +'</p>'+
-'</div>'+
-'<div class="card-footer">'+
- '<div class="text-center"><a href="#" class="btn btn-primary">Agregar al carrito</a>\n </div>'+
-'</div>'+
-'</div>'+
-'</div>';
+`<div class="card-group col-lg-3 col-sm-12 col-md-6 card-padding2">
+    <div class="card">
+        <img src="${item.img}" class="card-img-top mx-auto" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                    <p class="card-text">${item.price}</p>
+                    <p class="card-text">${item.description}</p>
+                    </div>
+                <div class="card-footer">
+            <div class="text-center"><button type="button" class="btn btn-outline-dark">Add to cart</button>\n </div>
+        </div>
+    </div>
+</div>`;
 
     const itemsContainer = document.getElementById("list-items");
     itemsContainer.innerHTML += itemHTML;
