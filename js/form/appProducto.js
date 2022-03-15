d678c923ee54dfc5d390d585bff8569d1f25d6f7
kingaurelio1 <aurelior997@ciencias.unam.mx>
2022-03-15 00:45:18 -0600

    Agrego el img fluid

--- js/form/appProducto.js
+++ js/form/appProducto.js
@@ -15,7 +15,7 @@ class UI {
     element.innerHTML = `
   
     <div class="card text-center mb-2 mt-3">
-    <img src="${producto.image}" class="card-img-top img-thumbnail" alt="...">  
+    <img src="${producto.image}" class="card-img-top img-thumbnail img-fluid" alt="...">  
       <div class="card-body"> 
         <strong>Producto</strong>: ${producto.name}
         <strong>Precio</strong>: ${producto.price}
