bf877e712850b8670e5f25035dcb3e32c5971c1f
fernanda3091 <maria.fernanda.rivera.medina30@gmail.com>
2022-03-18 00:29:37 -0600

    Se cambia el placeholder de confirmar contraseña en el formulario de crear cuenta

--- js/logInCliente.js
+++ js/logInCliente.js
@@ -2,14 +2,14 @@ const logInCliente = document.getElementById('logInCliente');
 const inputs = document.querySelectorAll('#logInCliente input');
 
 const expresiones ={
-  correo: /^[a-zA-Z0-9_.+-]+@[a-Za-Z0-9]+\.[a-zA-Z0-9.]+$/,
+  correo2: /^[a-zA-Z0-9_.+-]+@[a-Za-Z0-9]+\.[a-zA-Z0-9.]+$/,
   nombre: /^[a-zA-ZÁ-y\s]{1,40}$/, // permite letras, espacios y acentos
   apellidos: /^[a-zA-ZÁ-y\s]{1,40}$/, // permite letras, espacios y acentos
   telefono: /^\d{14}$/  //solo acepta 14 digitos
 }//expresiones 
 
 const campos = {
-  correo: false,
+  correo2: false,
   nombre: false,
   apellidos: false,
   telefono: false,
@@ -18,7 +18,7 @@ const campos = {
 const validarlogInCliente = (e) =>{
   switch(e.target.name){
     case "correo":
-      validarCampo(expresiones.correo, e.target, "correo");
+      validarCampo(expresiones.correo2, e.target, "correo");
       break;
       case "nombre":
         validarCampo(expresiones.nombre, e.target, "nombre");
@@ -49,3 +49,23 @@ const validarCampo = (expresion, input, campo) =>{
 		campos[campo] = false;
   }//else
 }//if
+
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById("password");
+	const inputPassword2 = document.getElementById("password2");
+
+	if(inputPassword1.value !== inputPassword2.value){
+		document.getElementById(`grupo__password2`).classList.add("formulario__grupo-incorrecto");
+		document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-correcto");
+		document.querySelector(`#grupo__password2 i`).classList.add("fa-times-circle");
+		document.querySelector(`#grupo__password2 i`).classList.remove("fa-check-circle");
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add("formulario__input-error-activo");
+		campos["password"] = false;
+	} else {
+		document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-incorrecto");
+		document.getElementById(`grupo__password2`).classList.add("formulario__grupo-correcto");
+		document.querySelector(`#grupo__password2 i`).classList.remove("fa-times-circle");
+		document.querySelector(`#grupo__password2 i`).classList.add("fa-check-circle");
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove("formulario__input-error-activo");
+		campos["password"] = true;
+	}
