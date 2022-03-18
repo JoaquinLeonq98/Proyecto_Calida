e21983612d04490852f23161167f484dbc0299a7
fernanda3091 <maria.fernanda.rivera.medina30@gmail.com>
2022-03-18 00:15:48 -0600

    Se trabaja en la validación del logInCliente.js

--- js/logInCliente.js
+++ js/logInCliente.js
@@ -1,80 +1,51 @@
-class Cliente {
-    constructor(correo, name, apellidos, telefono, password) {
-      this.correo = correo;
-      this.name = name;
-      this.apellidos = apellidos;
-      this.telefono = telefono;
-      this.password = password;
-    }
-  }//class Cliente
+const logInCliente = document.getElementById('logInCliente');
+const inputs = document.querySelectorAll('#logInCliente input');
 
-  class UI {
-    resetForm() {
-      document.getElementById("logInCliente").reset();
-    }
-  
-    deleteCrearCuenta(element) {
-      if (element.name === "delete") {
-        element.parentElement.parentElement.parentElement.remove();
-        this.showMesage('Producto elminado satisfactoriamente', 'danger')
-      }
-    }
-  
-    successCrearCuenta(element) {
-      if (element.name === "add") {
-        console.log(element.parentElement.parentElement.parentElement); // metodo pendiente para subir a base de datos
-          
-      }
-    }
-  
-    showMesage(message, cssClass) {
-      const div = document.createElement("div");
-      div.className = `alert alert-${cssClass} mt-2`;
-      div.appendChild(document.createTextNode(message));
-      //muestra en el DOM
-      const container = document.querySelector(".container");
-      const app = document.querySelector("#App");
-      container.insertBefore(div, app);
-      //metodo y funcion de temporizador para elminar la alerta 
-      setTimeout(function(){
-          document.querySelector('.alert').remove();
-      },2300);
-    }
-  }
+const expresiones ={
+  correo: /^[a-zA-Z0-9_.+-]+@[a-Za-Z0-9]+\.[a-zA-Z0-9.]+$/,
+  nombre: /^[a-zA-ZÁ-y\s]{1,40}$/, // permite letras, espacios y acentos
+  apellidos: /^[a-zA-ZÁ-y\s]{1,40}$/, // permite letras, espacios y acentos
+  telefono: /^\d{14}$/  //solo acepta 14 digitos
+}//expresiones 
 
-  const crearCliente = document.getElementById("logInCliente");
-crearCliente.addEventListener("submit", function (event) {
-  const correo = document.getElementById("correo2").value;
-  const name = document.getElementById("name").value;
-  const apellidos = document.getElementById("apellidos").value;
-  const telefono = document.getElementById("telefono").value;
-  const password = document.getElementById("password").value;
+const campos = {
+  correo: false,
+  nombre: false,
+  apellidos: false,
+  telefono: false,
+}//const campos
 
-  let b = {correo:correo,name:name,apellidos:apellidos,telefono:telefono,password:password}; //Creación de JSON
-  let b_user=JSON.stringify(b);
-  localStorage.setItem("Info",b_user); //Se agrega a localStorage
+const validarlogInCliente = (e) =>{
+  switch(e.target.name){
+    case "correo":
+      validarCampo(expresiones.correo, e.target, "correo");
+      break;
+      case "nombre":
+        validarCampo(expresiones.nombre, e.target, "nombre");
+      break;
+      case "apellidos":
+        validarCampo(expresiones.apellidos, e.target, "apellidos");
+        break;
+      case "telefono":
+        validarCampo(expresiones.telefono, e.target,'telefono');
+        break;
+  }//switch
+}//const validar
 
-
-
-  console.log(correo, name, apellidos, telefono, password);
-
-  const cliente = new Cliente(correo, name, apellidos, telefono, password);
-
-  const ui = new UI();
-  //condicion para la validacion de datos
-  if(correo === '' || name === ''|| apellidos === '' || telefono === '' || password === ''){
-    return ui.showMesage('Campos incompletos, favor de llenarlos', 'warning');
-  }//termina funcion de validacion 
-
-  
-  ui.resetForm();
-  ui.showMesage('producto añadido satisfactoriamente', 'success' );
-  event.preventDefault();
-});
-
-document
-  .getElementById("logInCliente")
-  .addEventListener("submit", function (event) {
-    const ui = new UI();
-    ui.successProduct(event.target);
-  });
+const validarCampo = (expresion, input, campo) =>{
+  if(expresion.test(input.value)){
+    document.getElementById(`grupo_${campo}`).classList.remove("logInCliente_grupo-incorrecto");
+    document.getElementById(`grupo__${campo}`).classList.add("logInCliente__grupo-correcto");
+		document.querySelector(`#grupo__${campo} i`).classList.add("fa-check-circle"); 
+		document.querySelector(`#grupo__${campo} i`).classList.remove("fa-times-circle");
+		document.querySelector(`#grupo__${campo} .logInCliente__input-error`).classList.remove("logInCliente__input-error-activo");
+		campos[campo] = true;
+  }else{
+    document.getElementById(`grupo__${campo}`).classList.add("logInCliente__grupo-incorrecto");
+		document.getElementById(`grupo__${campo}`).classList.remove("logInCliente__grupo-correcto");
+		document.querySelector(`#grupo__${campo} i`).classList.add("fa-times-circle");
+		document.querySelector(`#grupo__${campo} i`).classList.remove("fa-check-circle");
+		document.querySelector(`#grupo__${campo} .logInCliente__input-error`).classList.add("logInCliente__input-error-activo");
+		campos[campo] = false;
+  }//else
+}//if
