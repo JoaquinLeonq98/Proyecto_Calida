331f117854f3a32c6c87de008725016e16213293
manueldltp <manueldltpina@gmail.com>
2022-03-22 21:01:07 -0700

    se cambiaron los campos del formulario loginAdmin por los de bootstrap

--- js/loginAdmin.js
+++ js/loginAdmin.js
@@ -1,4 +1,23 @@
-const formulario = document.getElementById('loginAdmin');
+	// Example starter JavaScript for disabling form submissions if there are invalid fields
+	(function() {
+	  'use strict';
+	  window.addEventListener('load', function() {
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		var forms = document.getElementsByClassName('needs-validation');
+		// Loop over them and prevent submission
+		var validation = Array.prototype.filter.call(forms, function(form) {
+		  form.addEventListener('submit', function(event) {
+			if (form.checkValidity() === false) {
+			  event.preventDefault();
+			  event.stopPropagation();
+			}
+			form.classList.add('was-validated');
+		  }, false);
+		});
+	  }, false);
+	})();
+
+/*const formulario = document.getElementById('loginAdmin');
 const inputs = document.querySelectorAll('#loginAdmin input');
 
 const expresiones = {
@@ -65,4 +84,4 @@ formulario.addEventListener('submit', (e) => {
 	} else {
 		document.getElementById('formularioMensaje').classList.add('formularioMensaje-activo');
 	}
-});//formulario event listener
+});//formulario event listener*/
