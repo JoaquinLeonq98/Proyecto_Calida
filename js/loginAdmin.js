ba545d3ae068d49edcfc1d6ad0b958d283f996c5
manueldltp <manueldltpina@gmail.com>
2022-03-24 17:29:46 -0700

    contunúo de limpieza y comentario del código

--- js/loginAdmin.js
+++ js/loginAdmin.js
@@ -18,13 +18,6 @@ function prueba (usuario, contra){
     return b;
 }//function prueba
 
-class Administrador {
-    constructor(usuario, password) {
-      this.usuario = usuario;
-      this.password = password;
-	}
-}//class administrador
-
 class UI {
     resetForm() {
       document.getElementById("loginAdmin").reset();
@@ -45,9 +38,9 @@ class UI {
 	}
 }//class UI
 
-const crearAdmin = document.getElementById("loginAdmin");
+const crearAdmin = document.getElementById("loginAdmin"); //se trae el formulario 
 crearAdmin.addEventListener("submit", function (event) {
-	event.preventDefault();
+	event.preventDefault(); //
 	const usuario1 = document.getElementById("validationCustom01").value;
 	const password = document.getElementById("validationCustom02").value;
 	if (prueba(usuario1,password)){
@@ -59,114 +52,7 @@ crearAdmin.addEventListener("submit", function (event) {
 			title: 'Tu usuario o contraseña es inválido',
 			showConfirmButton: false,
 			timer: 2300,
-		});
+		}); //alerta en caso de campos incorrectos
 	}
+});//función validateUser
 
-  const administrador = new Administrador(usuario1, password);
-  console.log(usuario1, password);
-  console.log(Administrador);
-
-  const ui = new UI();
-  //condicion para la validacion de datos
- 	if(usuario1 === '' || password === ''){
-    return ui.showMesage('Campos incompletos, favor de llenarlos', 'warning');
-	}//termina funcion de validacion 
-
-});//función validateUser	
-
-/*document
-.getElementById("loginAdmin")
-.addEventListener("submit", function (event) {
-  const ui = new UI();
-  ui.successProduct(event.target);
-});*/
-
-
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-	/*(function() {
-	  'use strict';
-	  window.addEventListener('load', function() {
-		// Fetch all the forms we want to apply custom Bootstrap validation styles to
-		var forms = document.getElementsByClassName('needs-validation');
-		// Loop over them and prevent submission
-		var validation = Array.prototype.filter.call(forms, function(form) {
-		  form.addEventListener('submit', function(event) {
-			if (form.checkValidity() === false) {
-			  event.preventDefault();
-			  event.stopPropagation();
-			}
-			form.classList.add('was-validated');
-		  }, false);
-		});
-	  }, false);
-	})();
-
-/*const formulario = document.getElementById('loginAdmin');
-const inputs = document.querySelectorAll('#loginAdmin input');
-
-const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	//nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
-	// correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	// telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-}
-
-const campos = {
-    usuario: false,
-    password: false
-}//campos
-
-const validarFormulario = (e) => {
-    switch (e.target.name) {
-		case "usuario":
-			validarCampo(expresiones.usuario, e.target, 'usuario');
-		break;
-        case "password":
-			validarCampo(expresiones.password, e.target, 'password');
-			validarPassword2();
-		break;
-    }//swith
-}//validarFormulario
-
-const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
-		document.getElementById(`grupo__${campo}`).classList.remove('grupoFormulario-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('grupoFormulario-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formularioInputError`).classList.remove('formularioInputError-activo');
-		campos[campo] = true;
-	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('grupoFormulario-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('grupoFormulario-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__${campo} .formularioInputError`).classList.add('formularioInputError-activo');
-		campos[campo] = false;
-	}//else
-}//validarCampo
-
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
-    if(campos.usuario && campos.password){
-		formulario.reset();
-
-		document.getElementById('formularioMensajeExito').classList.add('formularioMensajeExito-activo');
-		setTimeout(() => {
-			document.getElementById('formularioMensajeExito').classList.remove('formularioMensajeExito-activo');
-		}, 5000);
-
-		document.querySelectorAll('.grupoFormulario-correcto').forEach((icono) => {
-			icono.classList.remove('grupoFormulario-correcto');
-		});
-	} else {
-		document.getElementById('formularioMensaje').classList.add('formularioMensaje-activo');
-	}
-});//formulario event listener*/
