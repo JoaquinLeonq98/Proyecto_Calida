4d5cea1206311af3f7c95e2ea35e005e4b4687b1
lbca <calberto.lopezbenitez@gmail.com>
2022-03-30 16:55:05 -0600

    Se crea script para insertar información, incluye insert de las categorias y los productos

---
+++ dataBase/scriptInsertsDB.sql
@@ -0,0 +1,16 @@
+/*Insert de las categorías de los productos*/
+INSERT INTO `calidadb`.`categoria` (`nombre`) VALUES ('planta');
+INSERT INTO `calidadb`.`categoria` (`nombre`) VALUES ('maceta');
+INSERT INTO `calidadb`.`categoria` (`nombre`) VALUES ('base');
+INSERT INTO `calidadb`.`categoria` (`nombre`) VALUES ('jabon');
+
+/*Insert de los productos*/
+INSERT INTO `calidadb`.`producto` (`nombre`, `precio`, `descripcion`, `imagen`, `inventario`, `categoria_idcategoria`) VALUES ('aglaonema roja', '151.00', 'Tamaño CH, de riego 1 vez a la semana, luz tipo resolana y semisombra.', ?, '10', '1');
+INSERT INTO `calidadb`.`producto` (`nombre`, `precio`, `descripcion`, `imagen`, `inventario`, `categoria_idcategoria`) VALUES ('aglaonema rosa', '151.00', 'Tamaño CH, de riego 1 vez a la semana, luz tipo resolana, semisombra y luz artificia.', ?, '10', '1');
+INSERT INTO `calidadb`.`producto` (`nombre`, `precio`, `descripcion`, `imagen`, `inventario`, `categoria_idcategoria`) VALUES ('amoena / amoena hoja reina', '79.00', 'Tamaño CH, de riego 1 o 2 veces por semana, luz tipo resolana, semisombra y soporta sol directo en pequeñas cantidades.', ?, '10', '1');
+INSERT INTO `calidadb`.`producto` (`nombre`, `precio`, `descripcion`, `imagen`, `inventario`, `categoria_idcategoria`) VALUES ('calathea burle marx', '37.00', 'Tamaño CH, de riego 1 o 2 veces por semana, manteniendo húmedo el sustrato, luz tipo resolana y semisombra.', ?, '10', '1');
+INSERT INTO `calidadb`.`producto` (`nombre`, `precio`, `descripcion`, `imagen`, `inventario`, `categoria_idcategoria`) VALUES ('calathea makoyana', '103.00', 'Tamaño CH, de riego 1 o 2 veces por semana, luz tipo resolana o semisombra.', ?, '10', '1');
+
+/* */
+
+
